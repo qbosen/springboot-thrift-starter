5b201798b3ac888005f6bc9a15c85469da32446a
qiubaisen <abosen@qq.com>
2020-08-17 17:58:20 +0800

    示例部分

---
+++ examples/calculator/client/src/main/java/top/abosen/rpc/thrift/examples/calculator/Application.java
@@ -0,0 +1,15 @@
+package top.abosen.rpc.thrift.examples.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qiubaisen
+ * @date 2020/8/17
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ examples/calculator/client/src/main/java/top/abosen/rpc/thrift/examples/calculator/thrift/CalculatorThriftClient.java
@@ -0,0 +1,8 @@
+package top.abosen.rpc.thrift.examples.calculator.thrift;
+
+/**
+ * @author qiubaisen
+ * @date 2020/8/17
+ */
+public interface CalculatorThriftClient {
+}
