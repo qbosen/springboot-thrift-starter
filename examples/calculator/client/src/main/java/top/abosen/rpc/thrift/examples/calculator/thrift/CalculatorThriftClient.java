5087db68aca750850180f8d06848959037363e12
qiubaisen <abosen@qq.com>
2020-08-19 18:08:28 +0800

    客户端测试

--- examples/calculator/client/src/main/java/top/abosen/rpc/thrift/examples/calculator/thrift/CalculatorThriftClient.java
+++ examples/calculator/client/src/main/java/top/abosen/rpc/thrift/examples/calculator/thrift/CalculatorThriftClient.java
@@ -1,8 +1,13 @@
 package top.abosen.rpc.thrift.examples.calculator.thrift;
 
+import top.abosen.rpc.thrift.client.annotation.ThriftClient;
+import top.abosen.rpc.thrift.client.common.ThriftClientAware;
+import top.abosen.rpc.thrift.examples.calculator.tutorial.Calculator;
+
 /**
  * @author qiubaisen
  * @date 2020/8/17
  */
-public interface CalculatorThriftClient {
+@ThriftClient
+public interface CalculatorThriftClient extends ThriftClientAware<Calculator.Client> {
 }
